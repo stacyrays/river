4258e65dcea3fa02ff3090d3de35a03c1955d3db
Stacy Suits <stacyrays@gmail.com>
2024-11-14 13:05:08 -0800

    component submits contact post and some initial validation added

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { Button } from '@mui/material';
+import Alert from '@mui/material/Alert';
 import useContactForm from '../hooks/useContactForm';
 import { submitData } from './SubmitData';
 
@@ -10,13 +11,17 @@ export type ContactValues = {
     name: string;
     email: string;
     message: string;
+    date: Date;
 }
 
 export default function Contact() {
+    const [status, setStatus] = React.useState<"success" | "error" | "info" | "warning">('info');
+
     const initialValues: ContactValues = {
         name: '',
         email: '',
-        message: ''
+        message: '',
+        date: new Date(),
     }
 
     React.useEffect(() => {
@@ -33,35 +38,32 @@ export default function Contact() {
     const { values, handleChange, onReset, handleSubmit} = useContactForm({initialValues});
 
     const submitForm = async (formValues: ContactValues) => {
-        console.log('submit happens');
         console.log('formValues', formValues)
-        // submitData(formValues);
+        if (!formValues.name || !formValues.email || !formValues.message) {
+            setStatus('error');
+            return;
+        }
         
         try {
-            const response = await fetch('https://script.google.com/macros/s/AKfycbxHUVN2ZX4irCLU7ljjeItfc1-9Uzl3gr0oMkjKyyyULRpRcwfj7J4VWaxHN2CFOFQ/exec', {
-                redirect: "follow",
+            const response = await fetch('http://localhost:3000/api/contacts', {
                 method: 'POST',
-                mode: 'no-cors',
-                // cache: "no-cache",
                 headers: {
-                    "Content-Type": "application/json;charset=UTF-8",
+                    "Content-Type": "application/json",
                   },
-                // body: JSON.stringify(formValues),
-                // @ts-ignore
                 body: JSON.stringify(formValues),
-                // body: {name: 'Jon', email: 'jon@gmail.com', message: 'ehyyy'},
             });
-            // console.log('response', response.json());
+            console.log('response', response.json());
             // const responseJson = await response.json();
             // console.log('responseJson', responseJson);
         
             if (!response.ok) {
                 throw new Error('Failed to submit data');
             }
-        
+            onReset();
+            setStatus('success');
             return response;
         } catch (error) {
-            console.log("error", error);
+            setStatus('error');
             throw new Error('Failed to submit data');
             // console.error(error); // Handle error
         }
@@ -69,33 +71,59 @@ export default function Contact() {
     }
     return (
         <div className="w-screen flex flex-col items-center justify-center">
+            {status === 'success' && (
+                <Alert
+                    variant="filled"
+                    severity={status}
+                    sx={{
+                        backgroundColor: '#4caf50',
+                        marginTop: '20px',
+                    }}
+                    onClose={() => setStatus('info')}
+                >
+                    Your message has been sent successfully.
+                </Alert>
+            )}
+            {status === 'error' && (
+                <Alert
+                    variant="filled"
+                    severity={status}
+                    sx={{
+                        backgroundColor: '#f44336',
+                        marginTop: '20px',
+                    }}
+                    onClose={() => setStatus('info')}
+                >
+                    Form submission failed. All fields required.
+                </Alert>
+            )}
             <div className="w-1/2 mt-20">
-            <form onSubmit={handleSubmit(submitForm)}>
-                <Stack spacing={4}>
-                    {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
-                    <TextField id="standard-basic" name="name" label="Name" variant="standard" value={values.name} onChange={handleChange} />
-                    <TextField id="standard-basic" name="email" label="Email" variant="standard" value={values.email} onChange={handleChange} />
-                    <TextField
-                        id="outlined-textarea"
-                        name="message"
-                        label="Message"
-                        placeholder="Enter message"
-                        value={values.message}
-                        onChange={handleChange}
-                        multiline
-                    />
-                    {/* <TextField id="filled-basic" label="Name" variant="filled" /> */}
-                    <div className="flex items-center justify-end">
-                        <Button type="submit" variant="contained" sx={{
-                            width: '140px',
-                            backgroundColor: 'rgb(245, 158, 11)',
-                            borderRadius: 12,
-                            boxShadow: 'none', 
-                            "&:hover": {
-                                bgcolor: "#D97708",
-                            },
-                        }}>Say Hello</Button>
-                    </div>
+                <form onSubmit={handleSubmit(submitForm)}>
+                    <Stack spacing={4}>
+                        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" /> */}
+                        <TextField id="standard-basic" name="name" label="Name" variant="standard" value={values.name} onChange={handleChange} />
+                        <TextField id="standard-basic" name="email" label="Email" variant="standard" value={values.email} onChange={handleChange} />
+                        <TextField
+                            id="outlined-textarea"
+                            name="message"
+                            label="Message"
+                            placeholder="Enter message"
+                            value={values.message}
+                            onChange={handleChange}
+                            multiline
+                        />
+                        {/* <TextField id="filled-basic" label="Name" variant="filled" /> */}
+                        <div className="flex items-center justify-end">
+                            <Button type="submit" variant="contained" sx={{
+                                width: '140px',
+                                backgroundColor: 'rgb(245, 158, 11)',
+                                borderRadius: 12,
+                                boxShadow: 'none', 
+                                "&:hover": {
+                                    bgcolor: "#D97708",
+                                },
+                            }}>Say Hello</Button>
+                        </div>
                 </Stack>
             </form>
             </div>
