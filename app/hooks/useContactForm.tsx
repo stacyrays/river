d6ef2e96cc741b7aedd928e6a00dd24b9231086d
Stacy Suits <stacyrays@gmail.com>
2025-01-05 11:04:46 -0800

    fix yet another eslint issue

--- app/hooks/useContactForm.tsx
+++ app/hooks/useContactForm.tsx
@@ -1,42 +1,42 @@
-import { useState, ChangeEvent } from 'react';
-import { ContactValues } from '../contact/page';
+// import { useState, ChangeEvent } from 'react';
+// import { ContactValues } from '../contact/page';
 
-const useContactForm = ({initialValues}: {initialValues: ContactValues}) => {
-    const [values, setValues] = useState(initialValues);
+// const useContactForm = ({initialValues}: {initialValues: ContactValues}) => {
+//     const [values, setValues] = useState(initialValues);
 
-    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
-        const {name, value} = e.target;
-        setValues({
-            ...values,
-            [name]: value,
-        })
-    };
+//     const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+//         const {name, value} = e.target;
+//         setValues({
+//             ...values,
+//             [name]: value,
+//         })
+//     };
 
-    const onReset = () => {
-        setValues(initialValues);
-    };
+//     const onReset = () => {
+//         setValues(initialValues);
+//     };
 
-    const handleSubmit = (callback: (formValues: ContactValues) => void) => (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        callback(values);
-    }
-    // const handleSubmit = async (e: React.FormEvent) => {
-    //     e.preventDefault();
+//     const handleSubmit = (callback: (formValues: ContactValues) => void) => (event: React.FormEvent<HTMLFormElement>) => {
+//         event.preventDefault();
+//         callback(values);
+//     }
+//     // const handleSubmit = async (e: React.FormEvent) => {
+//     //     e.preventDefault();
     
-    //     try {
-    //       const response = await axios.post('YOUR_GOOGLE_APPS_SCRIPT_URL', formData);
-    //       console.log(response.data); // Handle success
-    //     } catch (error) {
-    //       console.error(error); // Handle error
-    //     }
-    //   };
+//     //     try {
+//     //       const response = await axios.post('YOUR_GOOGLE_APPS_SCRIPT_URL', formData);
+//     //       console.log(response.data); // Handle success
+//     //     } catch (error) {
+//     //       console.error(error); // Handle error
+//     //     }
+//     //   };
 
-    return {
-        values,
-        handleChange,
-        onReset,
-        handleSubmit
-    }
-}
+//     return {
+//         values,
+//         handleChange,
+//         onReset,
+//         handleSubmit
+//     }
+// }
 
-export default useContactForm;
+// export default useContactForm;
