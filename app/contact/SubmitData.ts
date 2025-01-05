173326e5068b3fb682448d0938ce05f70ed877b1
Stacy Suits <stacyrays@gmail.com>
2025-01-05 11:03:14 -0800

    Fix another eslint issue

--- app/contact/SubmitData.ts
+++ app/contact/SubmitData.ts
@@ -1,19 +1,20 @@
 'use server';
-import { ContactValues } from './page'
+// Commented until can use contact form again
+// import { ContactValues } from './page'
 
-export const submitData = async (data: ContactValues) => {
+// export const submitData = async (data: ContactValues) => {
   
-    const response = await fetch('https://script.google.com/macros/s/AKfycbwWRGG5lWo-4rB-DnlfiUEcWuIwBTFVLbikU-uGrR0/dev', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
+//     const response = await fetch('https://script.google.com/macros/s/AKfycbwWRGG5lWo-4rB-DnlfiUEcWuIwBTFVLbikU-uGrR0/dev', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       },
+//       body: JSON.stringify(data),
+//     });
   
-    if (!response.ok) {
-      throw new Error('Failed to submit data');
-    }
+//     if (!response.ok) {
+//       throw new Error('Failed to submit data');
+//     }
   
-    return response.json();
-  }
+//     return response.json();
+//   }
